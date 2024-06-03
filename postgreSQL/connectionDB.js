22c1a827bb163da6f73ee6ae4deb97065353a176
diegospillo <123877541+diegospillo@users.noreply.github.com>
2024-06-03 19:27:19 +0200

    Update connectionDB.js

--- postgreSQL/connectionDB.js
+++ postgreSQL/connectionDB.js
@@ -3,9 +3,9 @@ const { Pool } = require("pg");
 function connection() {
     const pool = new Pool({
       user: "spillo",
-      host: "dpg-cn4sgj5jm4es73bqkaa0-a",
-      database: "pizza_76wm",
-      password: "Z7ynfw2tOXLMH8EvynoncMltgcTbLA9z",
+      host: "dpg-cpevoo63e1ms73ecvtng-a",
+      database: "pizza_76wm_9gwo",
+      password: "0paFkV7wE7LuGNLOlWqx0ODD1qZLyx2r",
       port: 5432,
     });
     return pool;
